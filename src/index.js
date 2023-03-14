a5e027908f8b8e53ebf9953cc95728a10f9ccdb4
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-03-15 00:48:49 +0200

    started hw

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+import axios from "axios";
+import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 import './css/styles.css';
 
-const DEBOUNCE_DELAY = 300;
+const refs = {
+    form: document.querySelector('.search-form'),
+    searchBtn: document.querySelector('.search-form'),
+}
+
+const KEY = "34416785-706900f4c4344fdefb158122c";
+const URL = `https://pixabay.com/api/?key={ KEY }&q={query}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+
+
+
